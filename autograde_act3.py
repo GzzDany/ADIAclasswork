1bd815898f591e9fb19cdd2d7568b6b91fc05fc2
GzzDany <60372312+GzzDany@users.noreply.github.com>
2025-02-11 04:07:45 -0600

    Update autograde_act3.py
    Corrected grade_p1

--- autograde_act3.py
+++ autograde_act3.py
@@ -8,28 +8,37 @@ def load_dataset():
   df = pd.read_parquet(url).astype(str)
   return df
 
-def grade_p1(gobals):
+def grade_p1(globals):
   if "columns_float" not in globals:
     print("Error, the variable columns_float is not defined.")
     return
+  columns_float = globals["columns_float"]
+  if type(columns_float) != list:
+    print("You should store a list in columns_float, defined using square brackets [] and separating items with commas.")
+  passed = True
+  columns_float_ans = ["passenger_count", "trip_distance", "RatecodeID", "fare_amount", "extra", "mta_tax", "tip_amount", "tolls_amount", "improvement_surcharge", "total_amount", "congestion_surcharge", "Airport_fee"]
+  for col in columns_float_ans:
+    if col not in columns_float:
+      print(f"You forgot to include {col} in columns_float. ")
+      passed = False
+
   if "columns_datetime" not in globals:
     print("Error, the variable columns_datetime is not defined.")
     return
-
-  passed = True
-  columns_float = globals["columns_float"]
   columns_datetime = globals["columns_datetime"]
-  ### check the float columns:
-  columns_float_ans = ["passenger_count", "trip_distance", "RatecodeID", "fare_amount", "extra", "mta_tax", "tip_amount", "tolls_amount", "improvement_surcharge", "total_amount", "congestion_surcharge", "Airport_fee"]
+  if type(columns_datetime) != list:
+    print("You should store a list in columns_datetime, defined using square brackets [] and separating items with commas.")
+  passed = True
   columns_datetime_ans = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
-
-  for col in columns_float_ans:
-    if col not in columns_float:
-      print(f"Your forgot to add {col} to your columns_float list!")
-      passed = False
   for col in columns_datetime_ans:
     if col not in columns_datetime:
-      prin(f"You forgot to add {col} to your columns_datetime list!")
+      print(f"You forgot to include {col} in columns_float. ")
       passed = False
+
   if passed:
-    print("Great job! Now you can move on to the next part.")
+    print("Well done! Now you can move on to the next section.")
+  else:
+    print("You should fix your code until you pass this section, because this will be required in the next part!")
+
+
+
