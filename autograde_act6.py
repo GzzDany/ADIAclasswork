d98d27a8b629d8de8dd092d555244fdb2aa62415
GzzDany <60372312+GzzDany@users.noreply.github.com>
2025-02-17 00:22:03 -0600

    Create autograde_act6.py

---
+++ autograde_act6.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+def load_dataset():
+  # URL to the dataset (Example: January 2024 Yellow Taxi Data in Parquet format)
+  url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet"
+
+  # Read the dataset directly into a Pandas DataFrame
+  df = pd.read_parquet(url)
+  df_clean = df.dropna()
+  df_clean = df_clean[df_clean["passenger_count"]>0]
+  df_clean = df_clean[df_clean["trip_distance"]>0]
+  df_clean = df_clean[df_clean["fare_amount"]>0]
+  df_clean = df_clean[df_clean["tip_amount"]>=0]
+  df_clean = df_clean[df_clean["total_amount"]>0]
+
+  
+  return df_clean
+
+def make_solution():
+  df_sol = load_dataset()
+  df_sol["trip_distance_km"] = df_sol["trip_distance"]* 1.60934
+  df_sol["tip_percentage"] = (df_sol["tip_amount"]/df_sol["fare_amount"])*100
+  df_sol["trip_duration"] = (df_sol["tpep_dropoff_datetime"] - df_sol["tpep_pickup_datetime"]).dt.total_seconds()/60
+  df_sol["hour_start"] = df_sol["tpep_pickup_datetime"].dt.hour
+  df_sol["weekday"] = df_sol["tpep_pickup_datetime"].dt.day_name()
+  df_sol["average_speed"] = df_sol["trip_distance_km"]/(df_sol["trip_duration"]/60)
+  df_sol["length_category"] = df_sol["trip_distance_km"].apply(categorize_length_sol)
+  df_sol["time_of_day"] = df_sol["hour_start"].apply(categorize_time_of_day_sol)
+  return df_sol
+
+
+df_sol = make_solution()
+
+def grade_p1(globals):
+  if "df" not in globals:
+    print("Error. Did you load the DataFrame into a variable called 'df'?")
+    return
+  else:
+    df = globals[df]
+  if df != df_sol:
+    print("You didn't load the proper file into the variable df. Try again!")
+  else:
+    print("Great!")
