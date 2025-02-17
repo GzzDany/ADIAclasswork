9685dd5ef67f386e7914c9e0354fc5f86fbd1437
GzzDany <60372312+GzzDany@users.noreply.github.com>
2025-02-17 01:26:21 -0600

    Update autograde_act6.py

--- autograde_act6.py
+++ autograde_act6.py
@@ -109,9 +109,67 @@ def grade_p5(globals):
     print("Try again. Did you extract the indices as instructed?")   
 
 
+def grade_p6(globals):
+  if "min_speed_index" not in globals:
+    print("Error. You did not record your answer into the variable 'min_speed_index'. Ddi you run the cell above?")
+    return
+  ans = df_sol["average_speed"].idxmin()
+  if math.isclose(globals["min_speed_index"], ans, abs_tol=0.1):
+    print("Good job!")
+  else:
+    print("Try again. Did you extract the index of the minimum value of the column 'average_speed'?")   
+
+
+def grade_p7(globals):
+  if "max_speed_index" not in globals:
+    print("Error. You did not record your answer into the variable 'max_speed_index'. Ddi you run the cell above?")
+    return
+  ans = df_sol["average_speed"].idxmax()
+  if math.isclose(globals["max_speed_index"], ans, abs_tol=0.1):
+    print("Good job!")
+  else:
+    print("Try again. Did you extract the index of the maximum value of the column 'average_speed'?")   
+
+
+def grade_p8(globals):
+  if "passengers_max_speed" not in globals:
+    print("Error. You did not record your answer into the variable 'passengers_max_speed'. Ddi you run the cell above?")
+    return
+  max_speed_index = df_sol["average_speed"].indmax()
+  ans = df_sol.loc[max_speed_index, "passenger_count"]
+  if math.isclose(globals["passengers_max_speed"], ans, abs_tol=0.1):
+    print("Good job!")
+  else:
+    print("Try again. Did you extract the index of the maximum speed, and then use it to select the passenger_count column with .iloc?")   
+
+
+def grade_p9(globals):
+  if "num_speed_2_3" not in globals:
+    print("Error. You did not record your answer into the variable 'num_speed_2_3'. Ddi you run the cell above?")
+    return
+  ans = df_sol[(df_sol["average_speed"]>2) & (df_sol["average_speed"]<3)].shape[0]
+  if math.isclose(globals["num_speed_2_3"], ans, abs_tol=0.1):
+    print("Good job!")
+  else:
+    print("Try again. Did you combine conditions using & and apply .shape to analyze how many rows were in the filtered DataFrame, or add the booleans of the combined masks using .sum()?")
+
+
+def grade_p10(globals):
+  if "mean_fare_vendor1" not in globals:
+    print("Error. You did not record your answer into the variable 'mean_fare_vendor1'. Ddi you run the cell above?")
+    return
+  ans = df_sol[(df_sol["VendorID"] == 1) & (df_sol["trip_distance"] >= 3)]["fare_amount"].mean()
+  if math.isclose(globals["mean_fare_vendor1"], ans, abs_tol=0.1):
+    print("Good job!")
+  else:
+    print("Try again. Did you combine the proper conditions, then select the 'fare_amount' column and finnally apply the .mean() method?")
 
 
 
 
 
-  
+
+
+
+
+
