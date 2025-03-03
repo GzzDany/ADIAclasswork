00e254cb35d9ff75de86c3459a17bace6c23835c
GzzDany <60372312+GzzDany@users.noreply.github.com>
2025-03-03 00:32:23 -0600

    Update autograde_act10.py

--- autograde_act10.py
+++ autograde_act10.py
@@ -7,6 +7,14 @@ from scipy.stats import shapiro, normaltest, anderson, probplot, mstats
 
 import pandas as pd
 
+def load_data_fbg():
+  df = pd.read_csv("blood_glucose.csv")
+  return df
+
+def load_data_height():
+  df = pd.read_csv("heights.csv")
+  return df
+
 def load_dataset_body():
   df = pd.read_csv("ADIAclasswork/body_measurements_clean.csv", index_col="Unnamed: 0")
   df.reset_index(inplace=True, drop=True)
