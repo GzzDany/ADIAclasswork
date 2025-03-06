87c707133f21a7b5222facb059a2eb3bb37b915f
GzzDany <60372312+GzzDany@users.noreply.github.com>
2025-03-06 00:57:53 -0600

    Update autograde_act12.py

--- autograde_act12.py
+++ autograde_act12.py
@@ -1,25 +1,25 @@
 import pandas as pd 
 
 def load_orders():
-  df1 = pd.read_csv("ADIAclasswork/orders1.csv", index_col="Unnamed 0: ")
-  df2 = pd.read_csv("ADIAclasswork/orders2.csv", index_col="Unnamed 0: ")
+  df1 = pd.read_csv("ADIAclasswork/orders1.csv", index_col="Unnamed 0:")
+  df2 = pd.read_csv("ADIAclasswork/orders2.csv", index_col="Unnamed 0:")
   return df1, df2
 
 def load_customers():
-  df1 = pd.read_csv("ADIAclasswork/customers1.csv", index_col="Unnamed 0: ")
-  df2 = pd.read_csv("ADIAclasswork/customers2.csv", index_col="Unnamed 0: ")
+  df1 = pd.read_csv("ADIAclasswork/customers1.csv", index_col="Unnamed 0:")
+  df2 = pd.read_csv("ADIAclasswork/customers2.csv", index_col="Unnamed 0:")
   return df1, df2
 
 def load_producst():
-  df = pd.read_csv("ADIAclasswork/olist_products_dataset.csv", index_col="Unnamed 0: ")
+  df = pd.read_csv("ADIAclasswork/olist_products_dataset.csv")
   return df
 
 def load_reviews():
-  df = pd.read_csv("ADIAclasswork/olist_order_reviews_dataset.csv", index_col="Unnamed 0: ")
+  df = pd.read_csv("ADIAclasswork/olist_order_reviews_dataset.csv")
   return df
 
 def load_order_items():
-  df = pd.read_csv("ADIAclasswork/olist_oder_items_dataset.csv", index_col="Unnamed 0: ")
+  df = pd.read_csv("ADIAclasswork/olist_oder_items_dataset.csv")
   return df
 
 
